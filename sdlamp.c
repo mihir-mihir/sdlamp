3ec0b2b4012820bf78deda80d14b8aa3cca9f9db
mihir <mihirsin@usc.edu>
2025-10-19 14:51:57 -0400

    after drag & drop video

--- sdlamp.c
+++ sdlamp.c
@@ -1,108 +1,135 @@
 #include "SDL.h"
 
 static SDL_AudioDeviceID audio_device = 0;
-static SDL_Window *window =
-    NULL; // any time you need to draw to screen in sdl, you need a window
-static SDL_Renderer *renderer = NULL;
+SDL_AudioSpec wavspec;
+static SDL_Window* window = NULL;  // any time you need to draw to screen in sdl, you need a window
+static SDL_Renderer* renderer = NULL;
 
 #if defined(__GNUC__) || defined(__clang__)
-static void panic_and_abort(const char *title, const char *text)
-    __attribute__((noreturn)); // c/c++ attributes
+static void panic_and_abort(const char* title, const char* text) __attribute__((noreturn));  // c/c++ attributes
 #endif
 
-static void panic_and_abort(const char *title, const char *text) {
-  fprintf(stderr, "PANIC: %s ... %s\n", title, text);
-  SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, title, text, NULL);
-  SDL_Quit();
-  exit(1);
+static void panic_and_abort(const char* title, const char* text) {
+    fprintf(stderr, "PANIC: %s ... %s\n", title, text);
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, title, text, window);
+    SDL_Quit();
+    exit(1);
 }
 
 int main() {
-  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1) {
-    panic_and_abort("SDL_Init failed", SDL_GetError());
-  }
+    Uint8* wavbuf = NULL;
+    Uint32 wavlen = 0;
 
-  window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_UNDEFINED,
-                            SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
-  if (!window) {
-    panic_and_abort("SDL_CreateWindow failed", SDL_GetError());
-  }
-
-  // renderer overlays things on created window - create opengl context?
-  // talk to the render in simple primitives
-  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-  if (!renderer) {
-    panic_and_abort("SDL_CreateRenderer failed", SDL_GetError());
-  }
-
-  SDL_AudioSpec wavspec;
-  Uint8 *wavbuf = NULL;
-  Uint32 wavlen = 0;
-  if (SDL_LoadWAV("music.wav", &wavspec, &wavbuf, &wavlen) == NULL) {
-    panic_and_abort("Couldn't load wav file!", SDL_GetError());
-  }
-
-#if 0 // !!! FIXME: come back to this
-    SDL_AudioSpec desired;
-    SDL_zero(desired);
-    desired.freq = 48000;
-    desired.format = AUDIO_F32;
-    desired.channels = 2;
-    desired.samples = 4096;
-    desired.callback = NULL;
-#endif
-
-  audio_device = SDL_OpenAudioDevice(NULL, 0, &wavspec, NULL, 0);
-  if (audio_device == 0) {
-    panic_and_abort("Could not open audio device!", SDL_GetError());
-  }
-
-  SDL_QueueAudio(audio_device, wavbuf, wavlen);
-
-  SDL_bool paused = SDL_TRUE;
-  const SDL_Rect rewind_rect = {110, 100, 100, 100};
-  const SDL_Rect pause_rect = {430, 100, 100, 100};
-
-  int green = 0;
-  SDL_bool keep_going = SDL_TRUE;
-  while (keep_going) {
-    SDL_Event e;
-    while (SDL_PollEvent(&e)) {
-      switch (e.type) {
-      case SDL_QUIT: {
-        keep_going = SDL_FALSE;
-        break;
-      }
-
-      case SDL_MOUSEBUTTONDOWN: {
-        const SDL_Point pt = {e.button.x, e.button.y};
-        if (SDL_PointInRect(&pt, &rewind_rect)) {
-          SDL_ClearQueuedAudio(audio_device);
-          SDL_QueueAudio(audio_device, wavbuf, wavlen);
-        } else if (SDL_PointInRect(&pt, &pause_rect)) {
-          paused = paused ? SDL_FALSE : SDL_TRUE;
-          SDL_PauseAudioDevice(audio_device, paused);
-        }
-        break;
-      }
-      }
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1) {
+        panic_and_abort("SDL_Init failed", SDL_GetError());
     }
 
-    SDL_SetRenderDrawColor(renderer, 0, green, 0, 255);
-    SDL_RenderClear(renderer);
+    // tells sdl we want this event type enabled it's disabled by default bc dropfile event triggers
+    // dynamic allocation of char* - memory will leak unless we free it explicitly with SDL_free when
+    // we're done with it
+    SDL_EventState(SDL_DROPFILE, SDL_ENABLE);
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    if (!window) {
+        panic_and_abort("SDL_CreateWindow failed", SDL_GetError());
+    }
 
-    SDL_RenderFillRect(renderer, &rewind_rect);
-    SDL_RenderFillRect(renderer, &pause_rect);
-    SDL_RenderPresent(renderer);
+    // renderer overlays things on created window - create opengl context?
+    // talk to the render in simple primitives
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer) {
+        panic_and_abort("SDL_CreateRenderer failed", SDL_GetError());
+    }
 
-    green = (green + 1) % 256;
-  }
+    SDL_bool paused = SDL_TRUE;
+    const SDL_Rect rewind_rect = {110, 100, 100, 100};
+    const SDL_Rect pause_rect = {430, 100, 100, 100};
 
-  SDL_FreeWAV(wavbuf);
-  SDL_CloseAudioDevice(audio_device);
-  SDL_Quit();
+    int green = 0;
+    SDL_bool keep_going = SDL_TRUE;
+    while (keep_going) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_QUIT: {
+                    keep_going = SDL_FALSE;
+                    break;
+                }
 
-  return 0;
+                case SDL_MOUSEBUTTONDOWN: {
+                    const SDL_Point pt = {e.button.x, e.button.y};
+                    if (SDL_PointInRect(&pt, &rewind_rect)) {
+                        // need to make sure that audio device exists - it won't if no file 
+                        // was dropped or if there was a problem opening one s
+                        if (audio_device) {
+                            SDL_ClearQueuedAudio(audio_device);
+                            SDL_QueueAudio(audio_device, wavbuf, wavlen);
+                        }
+                    } else if (SDL_PointInRect(&pt, &pause_rect)) {
+                        paused = paused ? SDL_FALSE : SDL_TRUE;
+                        if (audio_device) {
+                            SDL_PauseAudioDevice(audio_device, paused);
+                        }
+                    }
+                    break;
+                }
+
+                case SDL_DROPFILE: {
+                    if (audio_device) {
+                        SDL_CloseAudioDevice(audio_device);
+                        audio_device = 0;
+                    }
+                    SDL_FreeWAV(wavbuf);  // if you pass SDL_free a null it's a noop
+                    wavbuf = NULL;
+                    wavlen = 0;
+                    if (SDL_LoadWAV(e.drop.file, &wavspec, &wavbuf, &wavlen) == NULL) {
+                        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't load wav file", SDL_GetError(), window);
+                    }
+                    #if 0  // !!! FIXME: come back to this
+                    SDL_AudioSpec desired;
+                    SDL_zero(desired);
+                    desired.freq = 48000;
+                    desired.format = AUDIO_F32;
+                    desired.channels = 2;
+                    desired.samples = 4096;
+                    desired.callback = NULL;
+                    #endif
+                    audio_device = SDL_OpenAudioDevice(NULL, 0, &wavspec, NULL, 0);
+                    if (audio_device == 0) {
+                        SDL_ShowSimpleMessageBox(
+                                SDL_MESSAGEBOX_ERROR, "Couldn't open audio device", SDL_GetError(), window);
+                        SDL_FreeWAV(wavbuf);  // if you pass SDL_free a null it's a noop
+                        wavbuf = NULL;
+                        wavlen = 0;
+                    } else {
+                        SDL_QueueAudio(audio_device, wavbuf, wavlen);
+                        SDL_PauseAudioDevice(audio_device, paused);
+                    }
+                    SDL_free(e.drop.file);
+                    break;
+                }
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, green, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        SDL_RenderFillRect(renderer, &rewind_rect);
+        SDL_RenderFillRect(renderer, &pause_rect);
+        SDL_RenderPresent(renderer);
+
+        green = (green + 1) % 256;
+    }
+
+    SDL_FreeWAV(wavbuf);
+    SDL_CloseAudioDevice(audio_device);
+    
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
+    return 0;
 }
