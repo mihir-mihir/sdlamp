f42b9d161ee52471910f8757f96e2dc4d480bfe2
mihir <mihirsin@usc.edu>
2025-10-22 18:11:55 -0400

    my audio streams video implementation (seems like it works!)

--- sdlamp.c
+++ sdlamp.c
@@ -1,3 +1,24 @@
+/*
+NOTES FOR POST 16:20 AUDIO STREAMS VIDEO
+-----------------------------------------
+- general idea: implementing volume control involves making changes to the audio data in real time
+- going to feed audio device a little bit at a time instead of dumping the entire audio file in the queue
+- manipulating the audio will get complicated if allowed to use a lot of different formats,
+    fix a single format to work with
+- SDL_GetQueuedAudioSize -- how many bytes have I given to SDL to give to the audio device by calling SDL_QueueAudio
+- if less than 8 kB that we've given, need to feed more data (give 32 kB at a time for now? or however much is left if
+less)
+- SDL_assert
+- rewind button logic (clear queue, update wavpos), pause button logic
+- SDL_AudioStream global - init to NULL to be explicit
+    - this is how we "fake" desired audiospec?
+    - now only the stream needs to be global
+
+
+
+
+*/
+
 #include "SDL.h"
 
 static SDL_AudioDeviceID audio_device = 0;
@@ -18,28 +39,49 @@ static void panic_and_abort(const char* title, const char* text) {
 
 // THIS GLOBAL STATE IS NOT PERMANAENT
 // static variables in C are initialized to zero when declared
+// eventually might want to put these in a struct so one "thing" is getting passed around, not a lot of individual
+// globals
+
+static SDL_AudioStream* stream = NULL;
+static SDL_AudioSpec desired;
 static Uint8* wavbuf = NULL;
 static Uint32 wavlen = 0;
-static Uint32 wavpos = 0; // current byte position in wav file
-static SDL_AudioSpec wavspec;
+static Uint8* converted_buf[32 * 1024];  // 32 kB
 
-static SDL_bool open_new_audio_file(char* fname) {
+static SDL_bool init_audio_stream(char* fname) {
+    SDL_AudioSpec wavspec;
     SDL_FreeWAV(wavbuf);  // if you pass SDL_free a null it's a noop
     wavbuf = NULL;
-    wavlen = 0;
+    wavlen = 0;  // length in BYTES
     if (SDL_LoadWAV(fname, &wavspec, &wavbuf, &wavlen) == NULL) {
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't load wav file", SDL_GetError(), window);
         return SDL_FALSE;
     }
+
+    stream = SDL_NewAudioStream(
+            wavspec.format, wavspec.channels, wavspec.freq, desired.format, desired.channels, desired.freq);
     return SDL_TRUE;
 }
 
+static SDL_bool put_wavbuf_in_stream() {
+    SDL_AudioStreamClear(stream);
+    if (SDL_AudioStreamPut(stream, wavbuf, wavlen) == -1) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Couldn't put data in audio stream", SDL_GetError(), window);
+        return SDL_FALSE;
+    }
+    SDL_AudioStreamFlush(stream);  // convert all of the  data so it's available with SDL_AudioStreamGet
+    return SDL_TRUE;
+}
+
+static void send_audio_to_device_queue_from_stream() {
+    int gotten_bytes = SDL_AudioStreamGet(stream, converted_buf, sizeof(converted_buf));
+    if (gotten_bytes == -1) {
+        panic_and_abort("failed to get converted data", SDL_GetError());
+    }
+    SDL_QueueAudio(audio_device, converted_buf, gotten_bytes);
+}
 int main() {
-    Uint8* wavbuf = NULL;
-    Uint32 wavlen = 0;
-    SDL_AudioSpec wavspec;
-    sdfsdf;
-    SDL_AudioSpec desired;
+    SDL_bool paused = SDL_TRUE;
 
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1) {
         panic_and_abort("SDL_Init failed", SDL_GetError());
@@ -49,8 +91,6 @@ int main() {
     // dynamic allocation of char* - memory will leak unless we free it explicitly with SDL_free when
     // we're done with it
     SDL_EventState(SDL_DROPFILE, SDL_ENABLE);
-
-    open_new_audio_file("music.wav");
 
     window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
     if (!window) {
@@ -70,56 +110,73 @@ int main() {
     desired.channels = 2;
     desired.samples = 4096;
     desired.callback = NULL;
+
+    init_audio_stream("music.wav");
+    put_wavbuf_in_stream();
+
     // 2nd null arg is for obtained audiospec param, telling sdl that if hardware has issues with desired spec, make SDL
     // "fake" desired spec so that we can write code for desired spec (HAS to work with desired spec)
-    audio_device = SDL_OpenAudioDevice(NULL, 0, &wavspec, NULL, 0);
+    audio_device = SDL_OpenAudioDevice(NULL, 0, &desired, NULL, 0);
 
     if (audio_device == 0) {
         panic_and_abort("Couldn't open audio device", SDL_GetError());
     } else {
-        SDL_QueueAudio(audio_device, wavbuf, wavlen);
         SDL_PauseAudioDevice(audio_device, paused);
     }
 
-    SDL_bool paused = SDL_TRUE;
     const SDL_Rect rewind_rect = {110, 100, 100, 100};
     const SDL_Rect pause_rect = {430, 100, 100, 100};
 
     int green = 0;
     SDL_bool keep_going = SDL_TRUE;
     while (keep_going) {
+        int queued_bytes = SDL_GetQueuedAudioSize(audio_device);
+        if (queued_bytes < 8192) {
+            send_audio_to_device_queue_from_stream();
+        }
         SDL_Event e;
         while (SDL_PollEvent(&e)) {
             switch (e.type) {
-            case SDL_QUIT: {
-                keep_going = SDL_FALSE;
-                break;
-            }
-
-            case SDL_MOUSEBUTTONDOWN: {
-                const SDL_Point pt = {e.button.x, e.button.y};
-                if (SDL_PointInRect(&pt, &rewind_rect)) {
-                    // need to make sure that audio device exists - it won't if no file
-                    // was dropped or if there was a problem opening one s
-                    if (audio_device) {
-                        SDL_ClearQueuedAudio(audio_device);
-                        SDL_QueueAudio(audio_device, wavbuf, wavlen);
-                    }
-                } else if (SDL_PointInRect(&pt, &pause_rect)) {
-                    paused = paused ? SDL_FALSE : SDL_TRUE;
-                    if (audio_device) {
-                        SDL_PauseAudioDevice(audio_device, paused);
-                    }
+                case SDL_QUIT: {
+                    keep_going = SDL_FALSE;
+                    break;
                 }
-                break;
-            }
 
-            case SDL_DROPFILE: {
-                open_new_audio_file(e.drop.file);
-                sdfsdf;
-                SDL_free(e.drop.file);
-                break;
-            }
+                case SDL_MOUSEBUTTONDOWN: {
+                    const SDL_Point pt = {e.button.x, e.button.y};
+                    if (SDL_PointInRect(&pt, &rewind_rect)) {
+                        SDL_AudioStreamClear(stream);
+
+                        if (SDL_AudioStreamPut(stream, wavbuf, wavlen) == -1) {
+                            SDL_ShowSimpleMessageBox(
+                                    SDL_MESSAGEBOX_ERROR, "Couldn't put data in audio stream", SDL_GetError(), window);
+                        }
+                        // convert all of the  data so it's available with SDL_AudioStreamGet
+                        SDL_AudioStreamFlush(stream);
+
+                        int gotten_bytes = SDL_AudioStreamPut(stream, converted_buf, sizeof(converted_buf));
+
+                        // need to make sure that audio device exists - it won't if no file
+                        // was dropped or if there was a problem opening one
+                        if (audio_device) {
+                            SDL_ClearQueuedAudio(audio_device);
+                            SDL_QueueAudio(audio_device, converted_buf, gotten_bytes);
+                        }
+                    } else if (SDL_PointInRect(&pt, &pause_rect)) {
+                        paused = paused ? SDL_FALSE : SDL_TRUE;
+                        if (audio_device) {
+                            SDL_PauseAudioDevice(audio_device, paused);
+                        }
+                    }
+                    break;
+                }
+
+                case SDL_DROPFILE: {
+                    init_audio_stream(e.drop.file);
+                    put_wavbuf_in_stream();
+                    SDL_free(e.drop.file);
+                    break;
+                }
             }
         }
 
